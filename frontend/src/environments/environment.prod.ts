d31eb9fba1015a579f4347f4ad7fbbedd68cca0a
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-12-23 00:29:39 -0500

    removed extra slash

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    backendUrl: 'https://keyword-extraction-cc50365f2185.herokuapp.com/', // Backend URL for local development
+    backendUrl: 'https://keyword-extraction-cc50365f2185.herokuapp.com', // Backend URL for local development
   };
