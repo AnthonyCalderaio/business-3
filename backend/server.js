c4a6a6d5591b9485cc7620282d15dd183e6e0ca0
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-12-23 02:00:51 -0500

    plz work

--- backend/server.js
+++ backend/server.js
@@ -78,14 +78,22 @@ app.post('/extract-keywords', async (req, res) => {
 
 
 app.post('/webhook', express.raw({ type: 'application/json' }), (req, res) => {
+    console.log('Raw body:', req.body);  // Should be a Buffer object
+    console.log('Headers:', req.headers);  // Should contain stripe-signature
   const sig = req.headers['stripe-signature'];
+    console.log('Stripe Signature:', sig);  // Check if the signature is there
+
+    try {
         const event = stripe.webhooks.constructEvent(req.body, sig, stripeWebookSecret);
+        console.log('Event verified successfully:', event);
         res.status(200).send('Received');
     } catch (err) {
-        console.error(`Webhook Error: ${err.message}`);
-        res.status(400).send(`Webhook Error: ${err.message}`);
+        console.error('Error verifying webhook signature:', err);
+        res.status(400).send('Webhook error: ' + err.message);
   }
 });
+
+
 
 app.post('/create-customer', async (req, res) => {
     const { email, name } = req.body; // The user's email and name sent from the frontend
@@ -127,8 +135,8 @@ app.post('/create-checkout-session', async (req, res) => {
                 },
             ],
             mode: 'payment', // Payment mode
-            success_url: `${process.env.YOUR_FRONTEND_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
-            cancel_url: `${process.env.YOUR_FRONTEND_URL}/cancel`,
+            success_url: `${frontendUrl}/success?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `${frontendUrl}/cancel`,
             customer: customerId, // Pass the Stripe customer ID here to associate with the payment
         });
 
