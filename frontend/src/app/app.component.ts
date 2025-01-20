da0e98cbf50eb9f03aebb6f8c5b6f31bdfdfb72b
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2025-01-20 14:12:23 -0500

    cleanup

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -12,36 +12,5 @@ import { environment } from '../environments/environment';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  // title = 'frontend';
-  // inputText: string = '';
-  // keywords: any[] = [];
-  // isLoading: boolean = false;
-  // errorMessage: string = '';
-  // apiUrl = environment.backendUrl; 
-
-  // constructor(private http: HttpClient) {}
-
-  // extractKeywords(): void {
-  //   if (!this.inputText.trim()) {
-  //     this.errorMessage = 'Please enter some text.';
-  //     return;
-  //   }
-
-  //   this.isLoading = true;
-  //   this.errorMessage = '';
-
-  //   this.http.post<any>(`${this.apiUrl}`+'/extract-keywords', { text: this.inputText })
-  //     .subscribe({
-  //       next: (response) => {
-  //         this.keywords = response.keywords;
-  //         this.isLoading = false;
-  //       },
-  //       error: (error) => {
-  //         this.errorMessage = 'Error extracting keywords.';
-  //         this.isLoading = false;
-  //       }
-  //     });
-  // }
-}
+export class AppComponent { }
 
