e3927fefa8e75d6d03ebe54abf7b90a02dc9a467
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2025-01-24 18:04:18 -0500

    more stupidness

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
     auth0Domain: 'antfromclick.auth0.com',  // Will be populated dynamically in the app
     auth0ClientId: 'rJkVz_FNXR8Xe6fv91ddq82v5pHonfYh', // Will be populated dynamically in the app
     // Address for after login
-    auth0RedirectUri: 'https://seoextraction.com/home'
+    auth0RedirectUri: 'https://seoextraction.com/'
   };
